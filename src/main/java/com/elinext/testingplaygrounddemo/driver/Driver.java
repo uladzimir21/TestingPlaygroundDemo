6ffe0ef51213a19931c91563875038daa795625e
Uladzimir Hanin <uhanin@elinext.com>
2021-07-08 17:20:44 +0300

    feat: add webdrivermanager to the Driver class

--- src/main/java/com/elinext/testingplaygrounddemo/driver/Driver.java
+++ src/main/java/com/elinext/testingplaygrounddemo/driver/Driver.java
@@ -1,22 +1,20 @@
 package com.elinext.testingplaygrounddemo.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    private static final String DRIVER_NAME = "webdriver.chrome.driver";
-    private static String DRIVER_PATH = "./src/main/resources/drivers/chromedriver";
-
     private static WebDriver driver;
 
     private Driver() {
     }
 
     public static WebDriver getDriver() {
-        setupOSBasedDriverPath();
-        System.setProperty(DRIVER_NAME, DRIVER_PATH);
+        setupOSBasedWebDriverManager();
+
         if (driver == null) {
             driver = new ChromeDriver();
             setupImplicitWait(10);
@@ -24,9 +22,20 @@ public class Driver {
         return driver;
     }
 
-    private static void setupOSBasedDriverPath() {
-        if (System.getProperty("os.name").toLowerCase().contains("win"))
-            DRIVER_PATH += ".exe";
+    private static void setupOSBasedWebDriverManager() {
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            WebDriverManager
+                    .chromedriver()
+                    .arch64()
+                    .win()
+                    .setup();
+        } else {
+            WebDriverManager
+                    .chromedriver()
+                    .arch64()
+                    .linux()
+                    .setup();
+        }
     }
 
     private static void setupImplicitWait(long l) {
